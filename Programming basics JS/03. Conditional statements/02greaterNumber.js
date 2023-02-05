1eb123c969885dfa18d66d0d22d8ad31756ae764
lechev95 <lecho.lechev@gmail.com>
2023-02-05 16:47:15 +0200

    conditional statements tasks added

---
+++ Programming basics JS/03. Conditional statements/02greaterNumber.js
@@ -0,0 +1,14 @@
+function solve(input)
+{
+    let firstNum = Number(input[0]);
+    let secondNum = Number(input[1]);
+
+    if (firstNum > secondNum) {
+        console.log(firstNum);
+    }
+    else{
+        console.log(secondNum);
+    }
+}
+
+solve(["5", "6"]);
